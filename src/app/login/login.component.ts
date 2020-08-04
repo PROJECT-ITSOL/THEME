6eafa2d2599f42fb58dd6baf8cd4241510de41f6
Nhanbn98 <nhann98@gmail.com>
2020-08-04 09:24:28 +0700

    create-theme-login-done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss', './login-responsive.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.eventInput();
   }
 
+  eventInput() {
+    // debugger
+    let inputs = document.querySelectorAll('.input');
+    // console.log(inputs);
+    inputs.forEach((input) => {
+      input.addEventListener('focus', function () {
+        let parent = this.parentNode.parentNode;
+        parent.classList.add('focus');
+      });
+
+      input.addEventListener('blur', function () {
+        let parent = this.parentNode.parentNode;
+        if (this.value == '') {
+          parent.classList.remove('focus');
+        }
+      });
+    });
+  }
 }
