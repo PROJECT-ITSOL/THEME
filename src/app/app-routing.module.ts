ba366bc94efe86cecdbe5d26c1e012b909fb7164
Nhanbn98 <nhann98@gmail.com>
2020-08-05 15:10:07 +0700

    routing-login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // {path:'', component:LoginComponent}
+  {path:'loginAdmin',component:LoginComponent},
+  {path:'', redirectTo:'/loginAdmin',pathMatch:'full'}
   // {path:'/admin/home',component:HomeComponent}
 ];
 
