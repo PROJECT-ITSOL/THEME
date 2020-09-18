82586938d9efcadec9b39d7f08c70b0a6ca3604b
Nhanbn98 <nhanbn98@gmail.com>
2020-09-18 16:00:08 +0700

    fix-icon

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      if(this.authService.isLoggedIn()){
+      if(this.authService.isLoggedIn()){ 
         return true;
       }else{
         this.router.navigate(['/loginAdmin'])
