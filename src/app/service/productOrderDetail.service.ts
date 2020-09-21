b27894b178e5d4d107ec676cf826117f18a8b838
Nhanbn98 <nhanbn98@gmail.com>
2020-09-21 13:38:17 +0700

    fix-oder-detail

--- src/app/service/productOrderDetail.service.ts
+++ src/app/service/productOrderDetail.service.ts
@@ -4,37 +4,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Order } from '../ultis/order';
 
-@Injectable({ providedIn: 'root'}) export class ProductOrderDetailService {
-    private apiUrl= "http://localhost:8080/api/orderdetail";
+@Injectable({ providedIn: 'root' }) export class ProductOrderDetailService {
+    private apiUrl = "http://localhost:8080/api/orderdetail";
 
-    constructor(private http: HttpClient){}
-        getByIdProductOrderdetail(id:string): Observable<OderDetail[]>{
-            return this.http.get<OderDetail[]>(this.apiUrl+'?idOrder='+id);
-        }
-        getOrderById(id:string):Observable<Order[]>{
-            return this.http.get<Order[]>(this.apiUrl+'/'+id);
-        }
+    constructor(private http: HttpClient) { }
+    getByIdProductOrderdetail(id: string) {
+        // return this.http.get(this.apiUrl + '/' + id);
+        return this.http.get(this.apiUrl + '/' + id)
+    }
+    getOrderById(id: string): Observable<Order[]> {
+        return this.http.get<Order[]>(this.apiUrl + '/' + id);
+    }
 
-        getData(): Observable<OderDetail[]> {
-            return this.http.get<OderDetail[]>(this.apiUrl);  
+    getData(): Observable<OderDetail[]> {
+        return this.http.get<OderDetail[]>(this.apiUrl);
 
-        }
+    }
 
-        addOrderDetail(orderDetail){
-            return this.http.post(this.apiUrl+'/addOrder',orderDetail);
-        }
+    addOrderDetail(orderDetail) {
+        return this.http.post(this.apiUrl + '/addOrder', orderDetail);
+    }
 
-        editOrderDetail(id,orderDetail){
-            return this.http.put(this.apiUrl+'/update/'+id,orderDetail,{
-                responseType: 'text' as 'json',
-            });
-        }
+    editOrderDetail(id, orderDetail) {
+        return this.http.put(this.apiUrl + '/update/' + id, orderDetail, {
+            responseType: 'text' as 'json',
+        });
+    }
 
-        delete(id){
-            return this.http.delete(this.apiUrl+'/delete/'+id);
-        }
+    delete(id) {
+        return this.http.delete(this.apiUrl + '/delete/' + id);
+    }
 
-        search(key:string,page:number): Observable<OderDetail[]>{
-            return this.http.get<OderDetail[]>(this.apiUrl+'/search?name='+key+'&page='+page);
-        }
+    search(key: string, page: number): Observable<OderDetail[]> {
+        return this.http.get<OderDetail[]>(this.apiUrl + '/search?name=' + key + '&page=' + page);
+    }
 }
