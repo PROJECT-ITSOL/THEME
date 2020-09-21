5333b8ce056a9bb28dbe49c2a58e45c9394e9309
le yen <ledinhyen6886@gmail.com>
2020-09-21 14:26:02 +0700

    update supplier fontend

--- src/app/supplier/supplier.component.ts
+++ src/app/supplier/supplier.component.ts
@@ -1,8 +1,8 @@
-import { Product } from './../ultis/product';
-import { Supplier } from './../ultis/supplier';
-import { SupplierService } from './../service/supplier.service';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Product} from './../ultis/product';
+import {Supplier} from './../ultis/supplier';
+import {SupplierService} from './../service/supplier.service';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-supplier',
@@ -10,122 +10,109 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./supplier.component.scss']
 })
 export class SupplierComponent implements OnInit {
-  
+
   listSupp: Supplier[];
-  page:number=0;
-  pages:Array<number>;
-  totalSupp:number;
-  idDelete:number;
-  keyword:string;
-  listProduct:  Product[];
-  nameSupplier:string;
-  dataSupp:Array<any>;
-  supplier=new Supplier();
-  message:string;
+  page = 0;
+  pages: Array<number>;
+  totalSupp: number;
+  idDelete: number;
+  keyword: string;
+  listProduct: Product[];
+  nameSupplier: string;
+  dataSupp: Array<any>;
+  supplier = new Supplier();
+  message: string;
 
-  constructor(private supplierService : SupplierService) { }
+  constructor(private supplierService: SupplierService) {
+  }
 
-  setPage(i,event:any){
+  setPage(i, event: any) {
     event.preventDefault();
     this.page = i;
     this.getSupp();
-
   }
 
-  showEdit(item:Supplier){
+  showEdit(item: Supplier) {
 
-    this.supplier=item;
-    this.listProduct=item['products'];
-    this.nameSupplier=item['name'];
-    this.idDelete=item['idSupplier']
+    this.supplier = item;
+    this.listProduct = item.products;
+    this.nameSupplier = item.name;
+    this.idDelete = item.idSupplier;
   }
 
 
   ngOnInit(): void {
-<<<<<<< HEAD
     this.getSupp();
-
-=======
-     this.getSupp();
-    
->>>>>>> 36e879fceb67196b550f355fe609658418328277
+    this.getSupp();
   }
-   getSupp(){
+
+  /////////////////////////
+  getSupp() {
     this.listSupp = new Array();
     this.supplierService.getListSupp(this.page).subscribe(res => {
-
-    this.dataSupp=res['content'];
-    this.dataSupp.forEach((supp)=>{
-      let supplier = new Supplier();
-      supplier.idSupplier=supp['idSupplier'];
-      supplier.address=supp['address'];
-      supplier.logo=supp['logo'];
-      supplier.name=supp['name'];
-      supplier.status=supp['status'];
-      supplier.phoneNumber=supp['phoneNumber'];
-      supplier.products=supp['productList'];
-      this.listSupp.push(supplier);
-    });
-    this.pages = new Array(res['totalPages']);
-    this.totalSupp = (res['totalElements']);
-    });
-
-
-  }
-
-
-  onSubmit(form:NgForm){
-    this.supplierService.addSupp(form.value).subscribe((res) => {
-      this.message=res['message'];
-      location.reload();
-       alert(res['message']);
-
-    });
-  }
-  edit(form:NgForm){
-<<<<<<< HEAD
-    console.log('Your form data : ',form.value);
-    this.supplierService.editSupp(this.supplier['idSupplier'],form.value).subscribe(res => {
-
-      this.getSupp();
-
-=======
-    this.supplierService.editSupp(this.supplier['idSupplier'],form.value).subscribe(res => {    
-      this.getSupp(); 
->>>>>>> 36e879fceb67196b550f355fe609658418328277
-    });
-  }
-
-  delete(){
-    this.supplierService.delete(this.idDelete).subscribe(res =>{
-<<<<<<< HEAD
-      console.log(res);
-
-=======
->>>>>>> 36e879fceb67196b550f355fe609658418328277
-      alert(res['message']);
-      this.getSupp();
-    })
-  }
-
-  search(){
-    this.listSupp=new Array();
-    this.supplierService.search(this.keyword,this.page).subscribe(res =>{
-      this.dataSupp=res['content'];
-      this.dataSupp.forEach((supp)=>{
-        let supplier = new Supplier();
-        supplier.idSupplier=supp['idSupplier'];
-        supplier.address=supp['address'];
-        supplier.logo=supp['logo'];
-        supplier.name=supp['name'];
-        supplier.status=supp['status'];
-        supplier.phoneNumber=supp['phoneNumber'];
-        supplier.products=supp['productList'];
+      this.dataSupp = res.content;
+      this.dataSupp.forEach((supp) => {
+        const supplier = new Supplier();
+        supplier.idSupplier = supp.idSupplier;
+        supplier.address = supp.address;
+        supplier.logo = supp.logo;
+        supplier.name = supp.name;
+        supplier.status = supp.status;
+        supplier.phoneNumber = supp.phoneNumber;
+        supplier.products = supp.productList;
         this.listSupp.push(supplier);
       });
-      this.pages = new Array(res['totalPages']);
-      this.totalSupp = (res['totalElements']);
-      });
+      this.pages = new Array(res.totalPages);
+      this.totalSupp = (res.totalElements);
+    });
   }
 
+
+  onSubmit(form: NgForm) {
+    this.supplierService.addSupp(form.value).subscribe((res) => {
+      this.message = res.message;
+      location.reload();
+      alert(res.message);
+
+    });
+  }
+
+  edit(form: NgForm) {
+    console.log('Your form data : ', form.value);
+    this.supplierService.editSupp(this.supplier.idSupplier, form.value).subscribe(res => {
+      this.getSupp();
+    });
+    // tslint:disable-next-line:no-shadowed-variable
+    // this.supplierService.editSupp(this.supplier.idSupplier, form.value).subscribe(res => {
+    //   this.getSupp()
+    // });
+  }
+
+  delete() {
+    this.supplierService.delete(this.idDelete).subscribe(res => {
+      console.log(res);
+      alert(res.message);
+      this.getSupp();
+    });
+  }
+
+  search() {
+    this.listSupp = new Array();
+    this.supplierService.search(this.keyword, this.page).subscribe(res => {
+      this.dataSupp = res.content;
+      this.dataSupp.forEach((supp) => {
+        const supplier = new Supplier();
+        supplier.idSupplier = supp.idSupplier;
+        supplier.address = supp.address;
+        supplier.logo = supp.logo;
+        supplier.name = supp.name;
+        supplier.status = supp.status;
+        supplier.phoneNumber = supp.phoneNumber;
+        supplier.products = supp.productList;
+        this.listSupp.push(supplier);
+      });
+      this.pages = new Array(res.totalPages);
+      this.totalSupp = (res.totalElements);
+    });
+  }
 }
