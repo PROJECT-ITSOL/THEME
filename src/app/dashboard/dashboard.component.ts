cc9fca6441e2c40936c83e50f8de0c1b4aee606a
Nhanbn98 <nhanbn98@gmail.com>
2020-09-30 14:01:19 +0700

    config-dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { HttpParams } from '@angular/common/http';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 import { from } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Chart } from '../../../node_modules/chart.js';
@@ -8,126 +10,85 @@ import { draw, generate } from 'patternomaly';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  months: string[] = []
+  dataComment: number[] = []
+  years: number[] = []
+  monthNow: number=0
+  totalCategory:number=0
+  colors = [
+    '#1FDECF',
+    '#1FC6DE',
+    '#1FDE3A',
+    '#1FDE56',
+    '#F0845F',
+    '#F05F71',
+  ];
+  // LineChart
+  chartData = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: this.colors[1],
+        opacity: 0.5,
+        borderColor: this.colors[0],
+        borderWidth: 4,
+        pointBackgroundColor: this.colors[0],
+      },
+    ],
+  };
+
+  constructor(private service: AuthenticationService) { }
 
   ngOnInit(): void {
-    var colors = [
-      '#1FDECF',
-      '#1FC6DE',
-      '#1FDE3A',
-      '#1FDE56',
-      '#F0845F',
-      '#F05F71',
-    ];
-    // LineChart
-    var chartData = {
-      labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-      datasets: [
-        {
-          data: [589, 445, 483, 503, 689, 692, 634],
-          backgroundColor: colors[1],
-          opacity:0.5,
-          borderColor: colors[0],
-          borderWidth: 4,
-          pointBackgroundColor: colors[0],
-        },
-      ],
-    };
+    this.getTotalCategory()
+    this.getStatistical()
+  }
+
+  paintChart() {
     var myChart = document.getElementById('myAreaChart');
-    if (myChart) {
-      new Chart(myChart, {
-        type: 'line',
-        data: chartData,
-        options: {
-          scales: {
-            xAxes: [
-              {
-                gridLines: {
-                  color: 'transparent',
-                  zeroLineColor: 'transparent',
-                },
-                
+    var paint = new Chart(myChart, {
+      type: 'line',
+      data: this.chartData,
+      options: {
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                color: 'transparent',
+                zeroLineColor: 'transparent',
               },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: false,
-                },
+
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false,
               },
-            ],
-          },
-
-          legend: {
-            display: false,
-          },
+            },
+          ],
         },
-      });
-    }
 
-    //test
-    var colors = [
-      '#1FDECF',
-      '#1FC6DE',
-      '#1FDE3A',
-      '#1FDE56',
-      '#F0845F',
-      '#F05F71',
-    ];
-    // LineChart
-    var chartData = {
-      labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-      datasets: [
-        {
-          data: [589, 445, 483, 503, 689, 692, 634],
-          backgroundColor:'#FBFBEF' ,
-          opacity:0.5,
-          borderColor: colors[0],
-          borderWidth: 4,
-          pointBackgroundColor: colors[0],
+        legend: {
+          display: false,
         },
-        {
-          data: [111, 222, 333, 444, 89, 555, 455],
-          backgroundColor: colors[1],
-          opacity:0.5,
-          borderColor: colors[0],
-          borderWidth: 4,
-          pointBackgroundColor: colors[0],
-        },
-      ],
-    };
-    var myChart = document.getElementById('new');
-    if (myChart) {
-      new Chart(myChart, {
-        type: 'line',
-        data: chartData,
-        options: {
-          scales: {
-            xAxes: [
-              {
-                gridLines: {
-                  color: 'transparent',
-                  zeroLineColor: 'transparent',
-                },
-                
-              },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: false,
-                },
-              },
-            ],
-          },
+      },
+    })
 
-          legend: {
-            display: false,
-          },
-        },
-      });
-    }
+    // update data
+    // debugger
+    paint.data.datasets[0].data=[]
+    this.dataComment.forEach(e => {
+      paint.data.datasets[0].data.push(e)
+    })
+    paint.update()
 
+    paint.data.labels=[]
+    this.months.forEach(e => {
+      paint.data.labels.push(e)
+    })
+    paint.update()
 
     // Pie Chart
 
@@ -142,7 +103,7 @@ export class DashboardComponent implements OnInit {
       labels: ['Bootstrap', 'Popper', 'Other'],
       datasets: [
         {
-          backgroundColor: colors.slice(0, 3),
+          backgroundColor: this.colors.slice(0, 3),
           borderWidth: 0,
           data: [74, 11, 40],
         },
@@ -157,5 +118,50 @@ export class DashboardComponent implements OnInit {
         options: donutOptions,
       });
     }
+  }
+
+  getStatistical() {
+    let url = '/api/comment/statistical'
+    // this.years=new 
+    this.service.getStatisticalNoParam(url).subscribe(
+      res => {
+        let list = new Array()
+        list = res['data']['data']
+        this.monthNow=res['data']['monthNow']
+        this.months = res['data']['months']
+        this.years = res['data']['years']
+        list.forEach(data => {
+          this.dataComment.push(data)
+        })
+        this.paintChart()
+      }
+    )
+
+  }
+  setYears(event, year: number) {
+    event.preventDefault()
+    let url = "/api/comment/statistical"
+    const param = new HttpParams().append("year", year.toString())
+    this.service.getStatistical(url, param).subscribe(
+      res => {
+        let list = new Array()
+        this.dataComment=[]
+        list = res['data']['data']
+        list.forEach(e=>{
+          this.dataComment.push(e)
+        })
+        this.paintChart()
+        console.log(list)
+      }
+    )
+  }
+
+  getTotalCategory(){
+    let url="/api/category/total"
+    this.service.getTotalCategory(url).subscribe(
+      res=>{
+        this.totalCategory=res['data']
+      }
+    )
   }
 }
